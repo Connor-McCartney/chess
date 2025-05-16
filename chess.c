56fcee92c1ba4d914650c3ff97b57155e409a114
Connor McCartney <crm363@outlook.com>
2025-05-17 07:38:32 +1000

    highlight clicked pieces

--- chess.c
+++ chess.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <locale.h> 
+#include <stdio.h>
 #include "pieces.c"
 
 void draw_border(void) {
@@ -26,23 +27,104 @@ void draw_border(void) {
     mvwaddstr(stdscr, 0, 0, border);
 }
 
-void update_board(piece_t *board[8][8]) {
+void draw_board(square_t board[8][8]) {
     for (int i=0; i<8; i++) {
         for (int j=0; j<8; j++) {
-            mvwaddstr(stdscr, 15 - 2*j, 6 + 4*i, board[i][j]->unicode_char);
+            switch (board[i][j].highlight) {
+                case NORMAL:
+                    attrset(A_NORMAL);
+                    break;
+                case CLICKED:
+                    attrset(COLOR_PAIR(1));
+                    break;
+                case VALID_MOVE_DESTINATION:
+                    attrset(COLOR_PAIR(2));
+                    break;
+            }
+            piece_t piece = board[i][j].piece;
+            mvwaddstr(stdscr, 15 - 2*j, 6 + 4*i, piece.unicode_char);
+            refresh();
         }
     }
+    attrset(A_NORMAL);
     refresh();
 }
 
+void highlight_clicked_piece(square_t board[8][8], MEVENT event) {
+    int x, y;
+    x = (event.x-4)/4;
+    y = 7 - (event.y/2);
+
+    if (event.x < 5 || event.x > 35) {
+        // outside end border, invalid
+        x = -1; 
+    }
+
+    if (event.x % 4 == 0) {
+        // touching a border
+        x = -1; 
+    }
+
+    if (event.y > 15) {
+        // outside end border, invalid
+        y = -1;
+    }
+
+    if (event.y % 2 == 0) {
+        // touching a border
+        y = -1;
+    }
+
+    // remove all previous highlighty
+    for (int i=0; i<8; i++) {
+        for (int j=0; j<8; j++) {
+            board[i][j].highlight = NORMAL;
+        }
+    }
+
+    // highlight clicked piece
+    if (x != -1 && y != -1) {
+        board[x][y].highlight = CLICKED;
+    }
+}
+
 void run(void) {
+    start_color();
+    init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+
     curs_set(0); // hide cursor
     draw_border();
-    piece_t *board[8][8];
+    square_t board[8][8];
     initalise_board(board);
-    update_board(board);
+    draw_board(board);
 
-    getch();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    mousemask(ALL_MOUSE_EVENTS, NULL);
+    mouseinterval(0);
+
+    MEVENT event;
+    while (1) {
+        int c = getch();
+
+        if (c == 'q') {
+            return;
+        }
+
+        if (c == KEY_MOUSE) {
+            if (getmouse(&event) == OK) {
+                if (event.bstate & BUTTON1_PRESSED) {
+                    // left click
+                    highlight_clicked_piece(board, event);
+
+                } 
+            }
+        }
+        draw_board(board);
+        refresh();
+    }
 }
 
 
--- pieces.c
+++ pieces.c
@@ -1,6 +1,6 @@
 enum Colours {
-    WHITE, 
-    BLACK,
+    WHITE = 1, 
+    BLACK = -1,
 };
 
 enum Pieces {
@@ -12,11 +12,25 @@ enum Pieces {
     PAWN,
 };
 
+enum Highlights {
+    NORMAL,
+    CLICKED,
+    VALID_MOVE_DESTINATION,
+};
+
 typedef struct Piece {
     char *unicode_char;
     int colour;
     int piece;
 } piece_t;
+
+
+typedef struct Square{
+    piece_t piece;
+    int x;
+    int y;
+    int highlight;
+} square_t;
 
 
 struct Piece white_rook = {"♜", WHITE, ROOK};
@@ -33,35 +47,35 @@ struct Piece black_queen = {"♕", BLACK, QUEEN};
 struct Piece black_king = {"♔", BLACK, KING};
 struct Piece black_pawn = {"♙", BLACK, PAWN};
 
-struct Piece empty = {"", -1, -1};
+struct Piece empty = {" ", -4, -4};
 
-void initalise_board(piece_t *board[8][8]) {
+void initalise_board(square_t board[8][8]) {
     for (int i=0; i<8; i++) {
-        board[i][1] = &white_pawn;
-        board[i][6] = &black_pawn;
+        board[i][1].piece = white_pawn;
+        board[i][6].piece = black_pawn;
     }
 
     for (int i=0; i<8; i++) {
         for (int j=2; j<6; j++) {
-            board[i][j] = &empty;
+            board[i][j].piece = empty;
         }
     }
 
-    board[0][0] = &white_rook;
-    board[1][0] = &white_knight;
-    board[2][0] = &white_bishop;
-    board[3][0] = &white_queen;
-    board[4][0] = &white_king;
-    board[5][0] = &white_bishop;
-    board[6][0] = &white_knight;
-    board[7][0] = &white_rook;
+    board[0][0].piece = white_rook;
+    board[1][0].piece = white_knight;
+    board[2][0].piece = white_bishop;
+    board[3][0].piece = white_queen;
+    board[4][0].piece = white_king;
+    board[5][0].piece = white_bishop;
+    board[6][0].piece = white_knight;
+    board[7][0].piece = white_rook;
 
-    board[0][7] = &black_rook;
-    board[1][7] = &black_knight;
-    board[2][7] = &black_bishop;
-    board[3][7] = &black_queen;
-    board[4][7] = &black_king;
-    board[5][7] = &black_bishop;
-    board[6][7] = &black_knight;
-    board[7][7] = &black_rook;
+    board[0][7].piece = black_rook;
+    board[1][7].piece = black_knight;
+    board[2][7].piece = black_bishop;
+    board[3][7].piece = black_queen;
+    board[4][7].piece = black_king;
+    board[5][7].piece = black_bishop;
+    board[6][7].piece = black_knight;
+    board[7][7].piece = black_rook;
 }
